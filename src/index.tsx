a993d92b762bf2d2b9b0fbb799ed3cb9cc079df8
Irina77777777 <drdpi@ukr.net>
2022-12-20 19:21:31 +0200

    strilkova fumction

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <h1>Hello React</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit odit quas quibusdam quod eum iste totam inventore laborum, nostrum autem ratione. Maxime est numquam illum in corporis ratione hic nesciunt.</p>
-    </div>
+      <div>
+          <h1>Hello React</h1>
+          <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit
+              odit quas quibusdam quod eum iste totam inventore laborum, nostrum
+              autem ratione. Maxime est numquam illum in corporis ratione hic
+              nesciunt.
+          </p>
+          <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit
+              odit quas quibusdam quod eum iste totam inventore laborum, nostrum
+              autem ratione. Maxime est numquam illum in corporis ratione hic
+              nesciunt.
+          </p>
+      </div>
   )
 }
 
