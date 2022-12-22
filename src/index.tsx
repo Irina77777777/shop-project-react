2fb60201fe6a8eba6e71cf5163d3d8ad7e6c7396
Irina77777777 <drdpi@ukr.net>
2022-12-22 12:05:16 +0200

    base structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,8 @@
 import React, { Fragment } from 'react'
 import ReactDOM from 'react-dom/client'
-import { Title } from './Title'
 
 
-type ContentProps = {
-    p1: string
-    p2: string
-}
-
-
-
-
-const Content = (props: ContentProps) => {
-  return (
-      <React.Fragment>
-          <p>{props.p1}</p>
-          <p>{props.p2}</p>
-      </React.Fragment>
-  )
-}
-
-const App = () => {
-    return (
-        <>
-            <Title title="React" />
-            <Title title="TS" />
-            <Title title={10} />
-            <Content  p1="Hello React1" p2="Hello TS 222"/>
-        </>
-    )
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(<React.StrictMode>
- <App />
 </React.StrictMode>)
