2b32295b3d78d8e8590e7cea81b5429049468841
Irina77777777 <drdpi@ukr.net>
2023-01-13 21:49:58 +0200

    add arrReviews

--- src/components/Reviews/Reviews.tsx
+++ src/components/Reviews/Reviews.tsx
@@ -1,7 +1,44 @@
+import { Typography, Card, CardContent } from "@mui/material"
+import { useState } from "react"
+
 type Props = {}
+type Review = {
+  name: string
+  text: string
+}
 const Reviews = (props: Props) => {
-  return (
-    <div>Reviews</div>
-  )
+    const arrReviews: Review[] = [
+        {
+            name: 'Jack',
+            text: 'В нашем номере вид был на Dubai Marina (район с небоскрёбами, каналом и чуть море было видно) - невероятная красота, советую именно этот вид выбирать',
+        },
+        {
+            name: 'Ann',
+            text: 'У нас были только завтраки включены - были вкусные. Развлечений в самом отеле нет, есть только один бассейн, который особо нам и не нужен был',
+        },
+    ]
+
+    const [reviews, setReviews] = useState<Review[]>(arrReviews)
+    return (
+        <>
+            <Typography variant="h4" component={'h2'} sx={{ margin: '30px 0' }}>
+                Reviews
+            </Typography>
+            {reviews.map(({ name, text }: Review, i) => (
+                <Card
+                    variant="outlined"
+                    key={i}
+                    sx={{
+                        margin: '30px 0',
+                    }}
+                >
+                    <CardContent>
+                        <div>Name:{name}</div>
+                        <div>{text}</div>
+                    </CardContent>
+                </Card>
+            ))}
+        </>
+    )
 }
 export default Reviews
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,6 @@
 import { Typography } from "@mui/material"
 import ProductsList from "components/Products/ProductsList"
+import Reviews from "components/Reviews/Reviews"
 
 type Props = {
     addProductToCart: (id: number, count: number) => void
@@ -14,7 +15,8 @@ const Home = ({ addProductToCart }: Props) => {
       }} >
               List of Products{''}
       </Typography>
-      <ProductsList addProductToCart={addProductToCart}/>
+      <ProductsList addProductToCart={addProductToCart} />
+      <Reviews />
     </>
   )
 }
