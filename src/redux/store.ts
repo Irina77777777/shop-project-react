1fbd87f83ae20bef6198a07c748a7b43591ef390
Irina77777777 <drdpi@ukr.net>
2023-01-17 22:45:47 +0200

    add base cartReducer

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
+import cartReducer from './cartReducer'
 import likeReducer from './likeReducer'
 
 
 export const store = configureStore({
     reducer: {
-        likeProducts: likeReducer
+        likeProducts: likeReducer,
+        productsInCart: cartReducer
     },
 })
   
